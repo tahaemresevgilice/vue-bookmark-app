7cf5ed5f64195dd1081ccae2551820534eb4dcdb
tahaemresevgilice <tahaemresevgilice637@gmail.com>
2024-09-08 22:24:50 +0300

    Save current changes before switching branches

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+// import Input from "./components/Forms/Input.vue";
+// import Select from "./components/Forms/Select.vue";
+// import Label from "./components/Forms/Label.vue";
+import * as Form from "./components/form-components";
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <!-- <Label>
+    Başlık
+    <Input />
+  </Label>
+  <Label>
+    Kategori
+    <Select />
+  </Label> -->
+  <Form.Label>
+    Başlık
+    <Form.Input />
+  </Form.Label>
+  <Form.Label>
+    Kategori
+    <Form.Select />
+  </Form.Label>
+</template>
